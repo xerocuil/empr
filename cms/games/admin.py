6ba443c848ead134753d0580605cd37b3ca36b08
xerocuil <mail@xerocuil.com>
2021-10-30 00:26:57 -0400

    2021.1030

--- cms/games/admin.py
+++ cms/games/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Game, Genre, Platform, Tag
+from .models import Collection, Game, Genre, Platform, Tag
+
+def mark_as_archived(modeladmin, request, queryset):
+	queryset.update(archived=True)
+mark_as_archived.short_description = "Archived"
 
 class GameAdmin(admin.ModelAdmin):
-	list_display = ('title', 'genre', 'developer', 'publisher', 'platform')
+	list_display = ('title', 'genre', 'developer', 'publisher', 'platform', 'archived')
 	search_fields = (
 		'title',
 		'path',
@@ -12,7 +16,9 @@ class GameAdmin(admin.ModelAdmin):
 		'publisher',
 		'platform__name',
 	)
+	actions = [ mark_as_archived ]
 
+admin.site.register(Collection)
 admin.site.register(Game, GameAdmin)
 admin.site.register(Genre)
 admin.site.register(Platform)
