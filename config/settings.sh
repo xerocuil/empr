6d05c6d648c92ea32256040cd7e6851e2d87a656
xerocuil <mail@xerocuil.com>
2021-11-04 01:31:38 -0400

    2021.1104.0131

---
+++ config/settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Empr Settings
+APPDIR="/opt/empr"
+APPFILES="$HOME/.empr"
+APPNAME="$(basename $APPDIR)"
+CMSDIR="$APPDIR/cms"
+CACHEDIR="$APPFILES/cache"
+APPDB="$CMSDIR/db.sqlite3"
+CONFIGDIR="$APPDIR/config"
+UTILSDIR="$APPDIR/utils"
+VENV="$APPDIR/venv"
+
+## System Settings
+SQLITE="/usr/bin/sqlite3"
