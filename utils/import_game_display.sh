3c615631c3877ca79d9b22cce74d1e6f794567c6
xerocuil <tbrodhead@mm.st>
2021-11-06 21:18:18 -0400

    2021.1106.2118

--- utils/import_game_display.sh
+++ utils/import_game_display.sh
@@ -2,10 +2,6 @@
 
 . /opt/empr/config/settings.sh
 
-# FILE="$1"
-# game_data=$APPDIR/docs/csv/import_data.csv
-# cp "$FILE" "$game_data"
-
 . $VENV/bin/activate
 cd $CMSDIR
 python3 manage.py import_game_display
